1a81f7332ed750ae46816bd249ab4d63c28aab94
Phuong Tuan <phuongtuan8797@gmail.com>
2025-04-25 21:14:39 +0700

    remove laucher animation

--- clone_elfie/ios/Runner/AppDelegate.swift
+++ clone_elfie/ios/Runner/AppDelegate.swift
@@ -8,21 +8,6 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-//      window = UIWindow(frame: UIScreen.main.bounds)
-//          // Instantiate your LottieSplashViewController
-//          let splashViewController = LottieSplashViewController()
-//          window?.rootViewController = splashViewController // Start with the splash screen
-//          window?.makeKeyAndVisible()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
-//func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//    guard let windowScene = (scene as? UIWindowScene) else { return }
-//
-//    window = UIWindow(windowScene: windowScene)
-//    // Instantiate your LottieSplashViewController
-//    let splashViewController = LottieSplashViewController()
-//    window?.rootViewController = splashViewController // Start with the splash screen
-//    window?.makeKeyAndVisible()
-//}
